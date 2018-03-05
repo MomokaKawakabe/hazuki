cc6bafad1100a387f4e3a6080ef505538091d224
zhouyu <534153953@qq.com>
2018-03-06 07:44:09 +0800

    初始化项目：spring boot 1.5.11

---
+++ src/main/java/com/yinyin/hazuki/HazukiApplication.java
@@ -0,0 +1,12 @@
+package com.yinyin.hazuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazukiApplication.class, args);
+	}
+}
