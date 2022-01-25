4767428a207f3eefc9f02d2509ec5331633ea62e
zhouyu <534153953@qq.com>
2022-01-25 14:35:31 +0800

    init project

---
+++ src/main/java/com/momoka/hazuki/HazukiApplication.java
@@ -0,0 +1,13 @@
+package com.momoka.hazuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazukiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HazukiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/momoka/hazuki/HazukiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momoka.hazuki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazukiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
